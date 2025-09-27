d343aec50d7f089eb728a50af0acb7fb871b6f92
stoyannenchev <kaaremascode@gmail.com>
2025-09-27 09:41:13 +0300

    Commiting initial project

---
+++ src/main/java/com/example/employees_csv_app/EmployeesCsvAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.employees_csv_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesCsvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesCsvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employees_csv_app/EmployeesCsvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employees_csv_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesCsvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
